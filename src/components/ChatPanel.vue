3273f53f1883bb13bb08b3130f12950c7b948232
devedx <>
2023-06-13 20:00:33 +0200

    add markdown editor input, markdown rendering of messages, message editing, and response regeneration

--- src/components/ChatPanel.vue
+++ src/components/ChatPanel.vue
@@ -1,23 +1,37 @@
 <template>
 	<div class="chat-panel">
 		<div class="chat-messages" ref="messageContainer">
-			<div v-for="message in displayMessages" :key="message.id" :class="'message-container ' + (message.role === 'user' ? 'user' : 'assistant')">
-				<div :class="'message ' + (message.role === 'user' ? 'user' : 'assistant')" v-html="renderMarkdown(message.content)"></div>
+			<div v-for="(message, index) in displayMessages" :key="message.id" :class="'message-container ' + (message.role === 'user' ? 'user' : 'assistant')" @mouseover="hoveringMessage = message.id" @mouseleave="hoveringMessage = null">
+				<div v-if="editingMessage === message.id" :class="'message ' + (message.role === 'user' ? 'user' : 'assistant')">
+					<MdEditor v-model="message.content" class="message-editor" :theme="'dark'" :language="'en-US'" :preview="false" :toolbars="[]" :tab-width="4" :footers="[]" :no-upload-img="true" :auto-focus="true" />
+					<div class="edit-buttons">
+						<button v-if="message.role ==='user'" @click="saveEditAndRegenerate(index + 1)">Save & Regenerate</button>
+						<button @click="exitEdit">Save</button>
+						<button @click="cancelEdit(index + 1)" style="background-color: #777">Cancel</button>
+					</div>
+				</div>
+				<div v-else :class="'message preview ' + (message.role === 'user' ? 'user' : 'assistant')">
+					<MdPreview v-model="message.content" class="message-md-preview" :theme="'dark'" :language="'en-US'" :show-code-row-number="true" :preview-theme="'github'" :code-theme="'github'" />
+					<div class="message-buttons">
+						<button :disabled="hoveringMessage !== message.id || editingMessage !== null" class="icon-button" @click="editMessage(index + 1)"><font-awesome-icon icon="fa-solid fa-pen-to-square" /></button>
+						<button :disabled="hoveringMessage !== message.id || editingMessage !== null || message.role === 'user'" class="icon-button" @click="regenerate(index + 1)"><font-awesome-icon icon="fa-solid fa-rotate" /></button>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="input-container">
 			<div class="message-input">
 				<div class="input-type">
-					<button class="input-type-button" :class="{'input-type-active': !inputType}" @click="() => setInputType(false)"><font-awesome-icon icon="fa-solid fa-file-lines" class="input-type-icon" /></button>
+					<button class="icon-button" :class="{'input-type-active': !inputType}" @click="() => setInputType(false)"><font-awesome-icon icon="fa-solid fa-file-lines" class="input-type-icon" /></button>
 					<label class="switch">
-						<input type="checkbox" v-model="inputType" @change="test">
+						<input type="checkbox" v-model="inputType">
 						<span class="slider"></span>
 					</label>
-					<button class="input-type-button" :class="{'input-type-active': inputType }" @click="() => setInputType(true)"><font-awesome-icon icon="fa-brands fa-markdown" class="input-type-icon" /></button>
+					<button class="icon-button" :class="{'input-type-active': inputType }" @click="() => setInputType(true)"><font-awesome-icon icon="fa-brands fa-markdown" class="input-type-icon" /></button>
 				</div>
-				<div v-if="inputType"></div>
+				<MdEditor v-if="inputType" ref="mdInput" v-model="inputText" class="md-input" :placeholder="'Enter message ...'" :theme="'dark'" :language="'en-US'" :preview="false" :toolbars="[]" :tab-width="4" :footers="[]" :no-upload-img="true" />
 				<textarea v-else ref="input" class="input" rows="1" v-model="inputText" placeholder="Enter message ..." @input="resizeInput" @keypress.enter="handleInputEnter" @keydown.tab="handleInputTab"></textarea>
-				<button id="send-message" :disabled="sendDisabled" @click="sendMessage"><font-awesome-icon icon="fa-solid fa-paper-plane" class="send-icon" /></button>
+				<button id="send-message" class="icon-button" :disabled="sendDisabled" @click="sendMessage"><font-awesome-icon icon="fa-solid fa-paper-plane" class="send-icon" /></button>
 			</div>
 		</div>
 	</div>
@@ -27,17 +41,15 @@
 	import { ref, reactive, onMounted, watch, computed, nextTick } from "vue";
 	import { v4 as uuid } from "uuid";
 	import { fetchEventSource, EventStreamContentType } from "@microsoft/fetch-event-source";
-	import { remark } from "remark";
-	import remarkHtml from "remark-html";
-	import remarkGfm from "remark-gfm";
-	import remarkBreaks from "remark-breaks";
+	import { MdEditor, MdPreview, config } from "md-editor-v3";
+	import "md-editor-v3/lib/style.css";
 
 	const messages = reactive([{id: uuid(), role: "system", content: "You are ChatGPT, a large language model trained by OpenAI.Follow the user's instructions carefully. Respond using markdown. Your responses are to the point and not unnecessarily verbose. You do not apologize if you misunderstand the user or make a mistake."}]);
 	const displayMessages = computed(() => messages.filter(message => message.role !== "system"))
 	const gptMessages = computed(() => {
 		return messages.map(message => {
-			const {id, ...rest} = message;
-			return rest;
+			const {id, role, content, ...rest} = message;
+			return {role, content};
 		})
 	});
 
@@ -45,14 +57,29 @@
 	const messageContainer = ref(null);
 	const input = ref(null);
 	const inputType = ref(false);
+	const mdInput = ref(null);
+
+	const hoveringMessage = ref(null);
+	const editingMessage = ref(null);
+	let editingMessageOriginal = null;
 
 	onMounted(() => {
 		scrollToBottom(true);
-
-		//resizeInput();
+		config({ editorConfig: { renderDelay: 0 } });
 	});
 
 	watch(messages, () => scrollToBottom());
+
+	watch(inputType, () => {
+		nextTick(() => {
+			if (inputType.value) {
+				mdInput.value.focus();
+			} else {
+				input.value.focus();
+			}
+			scrollToBottom();
+		});
+	});
 
 	const sending = ref(false);
 	const sendDisabled = computed(() => inputText.value.trim() === "" || sending.value);
@@ -60,13 +87,14 @@
 	function resizeInput() {
 		input.value.style.height = "auto";
 		input.value.style.height = input.value.scrollHeight + "px";
+		scrollToBottom();
 	}
 
 	function scrollToBottom(force = false) {
 		const elem = messageContainer.value;
 
 		// don't scroll if force = false and messages are not scrolled to the bottom
-		if (elem.scrollTop + elem.clientHeight + 10 > elem.scrollHeight || force) {
+		if (elem.scrollTop + elem.clientHeight + 50 > elem.scrollHeight || force) {
 			nextTick(() => elem.scrollTop = elem.scrollHeight);
 		}
 	}
@@ -84,12 +112,12 @@
 		input.value.style.height = "auto";
 		scrollToBottom(true);
 
-		sending.value = false;
-		return;
+		generateResponse();
+	}
 
+	function generateResponse() {
 		// get assistant response
 		const url = "https://api.openai.com/v1/chat/completions";
-		messages.push({id: uuid(), role: "assistant", content: " |"});
 
 		fetchEventSource(url, {
 			method: "POST",
@@ -104,12 +132,13 @@
 			}),
 			onopen(response) {
 				if (response.ok && response.headers.get('content-type') === EventStreamContentType) {
+					messages.push({ id: uuid(), role: "assistant", content: " \u2588" });
 					return;
 				} else if (response.status >= 400 && response.status < 500 && response.status !== 429) {
 					throw new FatalError();
 				} else {
 					throw new Error();
-			}
+				}
 			},
 			onmessage(message) {
 				if (message.data === "[DONE]") {
@@ -120,7 +149,7 @@
 				const delta = JSON.parse(message.data).choices[0].delta;
 
 				if ("content" in delta)
-					messages[messages.length - 1].content = messages[messages.length - 1].content.substring(0, messages[messages.length - 1].content.length - 2) + delta.content + " |";
+					messages[messages.length - 1].content = messages[messages.length - 1].content.substring(0, messages[messages.length - 1].content.length - 2) + delta.content + " \u2588";
 			},
 			onclose() {
 				sending.value = false;
@@ -153,18 +182,34 @@
 		textarea.setSelectionRange(selectionStart + tab.length, selectionStart + tab.length);
 	}
 
-	function renderMarkdown(value) {
-		const processedContent = remark()
-			.use(remarkGfm)
-			.use(remarkBreaks)
-			.use(remarkHtml)
-			.processSync(value)
-			.toString();
-		return processedContent;
-	}
-
 	function setInputType(type) {
 		inputType.value = type;
+	}
+
+	function editMessage(index) {
+		editingMessageOriginal = messages[index].content;
+		editingMessage.value = messages[index].id;
+
+	}
+
+	function saveEditAndRegenerate(index) {
+		exitEdit();
+		regenerate(index + 1);
+	}
+
+	function exitEdit() {
+		editingMessage.value = null;
+		editingMessageOriginal = null;
+	}
+
+	function cancelEdit(index) {
+		messages[index].content = editingMessageOriginal;
+		exitEdit();
+	}
+
+	function regenerate(index) {
+		messages.splice(index);
+		generateResponse();
 	}
 
 	class FatalError extends Error {}
@@ -184,21 +229,62 @@
 
 	.message {
 		margin: 0 auto 0 auto;
-		padding: 20px 15px 20px 15px;
-		max-width: 780px;
-		line-height: 1.5;
+		padding-top: 15px;
+		max-width: 900px;
+	}
+
+	.message.preview {
+		display: flex;
+		align-items: flex-start;
+	}
+
+	.message-md-preview {
+		display: flex;
+		flex: 1;
 	}
 
 	.message.user {
-		padding-left: 100px;
+		padding-left: 60px;
+		max-width: 840px;
 	}
 
-	.message.assistant {
-		padding-right: 100px;
+	.message.user .md-editor {
+		--md-bk-color: #333;
+		--md-border-color: #333;
+		--md-scrollbar-bg-color: #333;
+		--md-color: #eee;
+	}
+
+	.message.assistant .md-editor {
+		--md-bk-color: #444;
+		--md-border-color: #444;
+		--md-scrollbar-bg-color: #444;
+		--md-color: #eee;
 	}
 
 	.message-container.assistant {
-		background-color: #555;
+		background-color: #444;
+	}
+
+	.message-editor {
+		height: auto;
+	}
+
+	.edit-buttons {
+		display: flex;
+		justify-content: center;
+		gap: 20px;
+		margin-right: 60px;
+		padding-bottom: 15px;
+	}
+
+	.edit-buttons button {
+		border: none;
+		border-radius: 5px;
+		padding: 10px 15px 10px 15px;
+		background-color: #9f6af1;
+		color: inherit;
+		cursor: pointer;
 	}
 
 	.message-input {
@@ -211,12 +297,6 @@
 		background-color: #444;
 		align-items: flex-end;
 
-	}
-
-	.custom-textarea .CodeMirror {
-		font: inherit;
-		color: inherit;
-		background: none;
 	}
 
 	.input {
@@ -240,12 +320,31 @@
 		color: #999;
 	}
 
-	.input-type-button {
+	.icon-button {
 		padding: 0;
 		border: none;
 		background: none;
 		color: #eee;
 		cursor: pointer;
+		font-size: 20px;
+	}
+
+	.message-buttons {
+		margin-top: 12px;
+	}
+
+	.message-buttons .icon-button {
+		color: #aaa;
+		padding-right: 10px;
+	}
+
+	.message-buttons .icon-button:hover {
+		color: inherit;
+	}
+
+	.message-buttons .icon-button:disabled {
+		color: transparent;
+		cursor: default;
 	}
 
 	.input-type-active {
@@ -254,9 +353,6 @@
 	}
 
 	#send-message {
-		padding: 0;
-		border: none;
-		background: none;
 		color: #9f6af1;
 	}
 
@@ -269,12 +365,8 @@
 	}
 
 	#send-message:disabled {
-		color: #888;
+		color: #666;
 		cursor: default;
-	}
-
-	.send-icon, .input-type-icon {
-		font-size: 20px;
 	}
 
 	.input-type {
@@ -323,5 +415,19 @@
 
 	input:checked + .slider:before {
 		transform: translateX(14px);
+	}
+
+	.md-input {
+		--md-color: #eee;
+		--md-bk-color: #444;
+		--md-border-color: #444;
+		--md-scrollbar-bg-color: #444;
+		--md-scrollbar-thumb-color: #777;
+		--md-scrollbar-thumb-hover-color: #aaa;
+		--md-scrollbar-thumb-active-color: #aaa;
+		display: flex;
+		max-height: 200px;
+		flex: 1;
+		margin-right: 15px;
 	}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import App from "./App.vue";
 //font awesome
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faPaperPlane, faFileLines } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane, faFileLines, faPenToSquare, faRotate } from "@fortawesome/free-solid-svg-icons";
 import { faMarkdown } from "@fortawesome/free-brands-svg-icons";
 
-library.add(faPaperPlane, faFileLines, faMarkdown);
+library.add(faPaperPlane, faFileLines, faMarkdown, faPenToSquare, faRotate);
 
 createApp(App)
 	.component("font-awesome-icon", FontAwesomeIcon)
